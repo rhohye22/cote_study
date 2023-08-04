a7def2732c81988472055229b5dde74db73411fb
rhohye22 <nhw666@gmail.com>
2023-08-04 11:36:11 +0900

    #BJ_11727

---
+++ src/BJ_11727/Main.java
@@ -0,0 +1,27 @@
+package BJ_11727;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(bf.readLine());
+
+		int[] dp = new int[n + 1];
+		dp[0] =1;
+		dp[1] = 1;
+		
+
+		for (int i = 2; i <= n; i++) {
+			dp[i] = (dp[i - 2]*2 + dp[i - 1]) % 10007;
+
+		}
+
+		System.out.println(dp[n]);
+	}
+}
